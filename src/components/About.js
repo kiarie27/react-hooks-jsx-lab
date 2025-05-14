5bd14db347b28aad19482d4fdc6e6f04b904dcdf
anthony kiarie <akiarie27@gmail.com>
2025-05-14 18:45:43 +0300

    final changes and tests

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { image } from "../data/data";
+import { image, about } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <img src={image} alt="I made this" />
+      <p>{about}</p>
+    </div>
+  );
 }
 
 export default About;
+
